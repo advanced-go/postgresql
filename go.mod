f4dcc71c88d23467743cfc814107db6475527e67
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-01 08:09:10 -0500

    Updating dependencies

--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/go-ai-agent/postgresql
 go 1.18
 
 require (
-	github.com/go-ai-agent/core v0.0.0-20231031155602-c6ddce196513
+	github.com/go-ai-agent/core v0.0.0-20231101124801-b49ec0b96fd0
 	github.com/jackc/pgx/v5 v5.2.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -18,6 +17,4 @@ require (
 	golang.org/x/crypto v0.10.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/text v0.10.0 // indirect
-	google.golang.org/grpc v1.56.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
 )
--- pgxsql/z_package.go
+++ pgxsql/z_package.go
@@ -2,6 +2,7 @@ package pgxsql
 
 import (
 	"github.com/go-ai-agent/core/runtime"
+	"net/http"
 	"reflect"
 	"sync/atomic"
 )
@@ -25,4 +26,47 @@ func setStarted() {
 
 func resetStarted() {
 	atomic.StoreInt64(&started, 0)
+}
+
+// newTypeHandler - templated function providing a TypeHandlerFn via a closure
+func newTypeHandler[E runtime.ErrorHandler]() runtime.TypeHandlerFn {
+	return func(r *http.Request, body any) (any, *runtime.Status) {
+		return typeHandler[E](r, body)
+	}
+}
+
+func typeHandler[E runtime.ErrorHandler](r *http.Request, body any) (any, *runtime.Status) {
+	//var e E
+
+	if r == nil {
+		return nil, runtime.NewStatus(http.StatusBadRequest)
+	}
+	// create a new context with a request id. Not creating a new request as upstream processing doesn't
+	// use http
+	/*
+		requestId := runtime.GetOrCreateRequestId(r)
+		nc := runtime.ContextWithRequestId(r.Context(), requestId)
+		switch r.Method {
+		case http.MethodGet:
+			entries, status := get(nc, r.Header.Get(httpx.ContentLocation), r.URL.Query())
+			if !status.OK() {
+				e.HandleStatus(status, requestId, locTypeHandler)
+				return nil, status
+			}
+			if entries == nil {
+				status.SetCode(http.StatusNotFound)
+			}
+			return entries, status
+		case http.MethodPut:
+			cmdTag, status := put(nc, r.Header.Get(httpx.ContentLocation), body)
+			if !status.OK() {
+				e.HandleStatus(status, requestId, locTypeHandler)
+				return nil, status
+			}
+			return cmdTag, status
+		default:
+		}
+
+	*/
+	return nil, runtime.NewStatus(http.StatusMethodNotAllowed)
 }
