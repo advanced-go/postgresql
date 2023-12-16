4cc7685e8b0eb58433c911b84eed847e6f30cbd6
go-ai-agent <mark.wilsonai@outlook.com>
2023-12-16 04:32:33 -0600

    Update package.go

--- pgxsql/package.go
+++ pgxsql/package.go
@@ -15,11 +15,11 @@ const (
 	PkgPath = "github.com/advanced-go/postgresql/pgxsql"
 )
 
-// Credentials - credentials function for authentication
-type Credentials func() (username string, password string, err error)
+// StartupCredentials - credentials function for authentication
+type StartupCredentials func() (username string, password string, err error)
 
-// Resource - database URL for connectivity configuration
-type Resource struct {
+// StartupResource - database URL for connectivity configuration
+type StartupResource struct {
 	Uri string
 }
 
